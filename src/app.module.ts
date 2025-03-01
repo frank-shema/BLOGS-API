7a174e37d99d7ec1b33221bee0e4024b461ce8c2
frank-shema <shemafranko0987@gmail.com>
2025-03-01 21:37:17 +0200

    started blog-api

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [DatabaseModule, AuthModule, UsersModule],
+})
+export class AppModule {}
