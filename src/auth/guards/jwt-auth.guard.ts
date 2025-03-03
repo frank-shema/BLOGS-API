6a4f55f191193c3c51c0a40db9c73dc4ab92a7b9
frank-shema <shemafranko0987@gmail.com>
2025-03-03 09:48:27 +0200

    upadted the guard jwt-auth

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,4 +1,8 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
@@ -11,10 +15,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err: any, user: any, info: any) {
+  handleRequest(err: any, user: any): any {
     // Customize the error handling or user extraction logic
     if (err || !user) {
-      throw err || new UnauthorizedException('Invalid or expired token');
+      if (err) {
+        throw err;
+      }
+      throw new UnauthorizedException('Invalid or expired token');
     }
     return user;
   }
