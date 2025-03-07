0aa37c9445dd21bd70b186cd0713d813a24b017e
frank-shema <shemafranko0987@gmail.com>
2025-03-07 22:42:55 +0200

    added shape controller

--- src/shape/shape.controller.ts
+++ src/shape/shape.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ShapeService } from './shape.service';
 
 @Controller('shape')
-export class ShapeController {}
+export class ShapeController {
+  constructor(private readonly shapeService: ShapeService) {}
+
+  @Get('square')
+  createSquare(@Query('sideLength') sideLength: number) {
+    return this.shapeService.createSquare(Number(sideLength));
+  }
+
+  @Get('rectangle')
+  createRectangle(
+    @Query('width') width: number,
+    @Query('height') height: number,
+  ) {
+    return this.shapeService.createRectangle(Number(width), Number(height));
+  }
+
+  @Get('circle')
+  createCircle(@Query('radius') radius: number) {
+    return this.shapeService.createCircle(Number(radius));
+  }
+}
